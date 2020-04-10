0190dbc09ccbec341aaf3197f72c131423f43b45
osluocra <osluocra@icloud.com>
2020-04-09 23:29:49 -0500

    Chapter 42 Complete. Enterprise Integration pattern. Content Based Router

---
+++ src/test/java/com/learncamel/eip/contentbasedrouter/ContentBaseRouteTest.java
@@ -0,0 +1,30 @@
+package com.learncamel.eip.contentbasedrouter;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+
+import java.io.File;
+
+public class ContentBaseRouteTest extends CamelTestSupport {
+    @Override
+    protected RoutesBuilder createRouteBuilder() throws Exception {
+        return new ContentBaseRoute();
+    }
+
+    @Test
+    public void ContentBaseRouteTest() throws InterruptedException {
+        Thread.sleep(5000);
+
+        File html = new File("data/output/html");
+        File txt = new File("data/output/txt");
+        File json = new File("data/output/json");
+        File other = new File("data/output/other");
+
+        assertTrue(html.isDirectory());
+        assertTrue(json.isDirectory());
+        assertTrue(txt.isDirectory());
+        assertTrue(other.isDirectory());
+
+    }
+}
