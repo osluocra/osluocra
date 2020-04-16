8d0ed6341ba00a0c0f5f9e23c8226b387839a5c4
osluocra <osluocra@icloud.com>
2020-04-15 23:58:23 -0500

    Lab 56 completed. How to insert into Postgres DB using camel part 1

---
+++ src/main/java/com/learncamel/routes/jdbc/DBPostgresRoute.java
@@ -0,0 +1,15 @@
+package com.learncamel.routes.jdbc;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class DBPostgresRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("direct:dbInput")
+                .to("log:?level=INFO&showBody=true")
+                .process(new InsertProcessor())
+                .to("jdbc:myDataSource");
+
+    }
+}
